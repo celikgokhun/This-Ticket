bfef5608111c5fe3b89f61ef6525ef05fef90102
celikgokhun <36249138+celikgokhun@users.noreply.github.com>
2021-09-26 17:44:00 +0300

    the end but. it may need retouches!

--- app/src/main/java/com/celik/gokhun/obilet/thisticket/view/MainActivity.kt
+++ app/src/main/java/com/celik/gokhun/obilet/thisticket/view/MainActivity.kt
@@ -15,19 +15,27 @@ import com.celik.gokhun.obilet.thisticket.util.getCurrentDateTomorrow
 import com.celik.gokhun.obilet.thisticket.util.getCurrentDateWithFineFormat
 import com.celik.gokhun.obilet.thisticket.util.getCurrentDateWithFineFormatTomorrow
 import com.celik.gokhun.obilet.thisticket.viewmodel.ViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var viewModel : ViewModel
 
     private lateinit var fromSpinner: Spinner
+    private lateinit var flightFromSpinner: Spinner
     private lateinit var toSpinner: Spinner
+    private lateinit var flightToSpinner: Spinner
 
     private lateinit var dateTextView: TextView
 
     private lateinit var  swipeRefresh : SwipeRefreshLayout
+    private lateinit var  swipeRefreshFlight : SwipeRefreshLayout
 
     private lateinit var dateFor: String
+
+    private lateinit var returnSection: LinearLayout
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +52,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         viewModel.refreshSessionData()
 
         swipeRefresh = findViewById(R.id.swipeToRefresh)
+        swipeRefreshFlight = findViewById(R.id.swipeRefreshFlight)
+        returnSection = findViewById(R.id.returnLayout)
 
 
         swipeRefresh.setOnRefreshListener{
@@ -51,11 +61,11 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         }
 
         Handler().postDelayed({
-            fillSpinners()},
-            2000)
+            fillSpinners()},2000)
+
+
 
     }
-
 
     private fun fillSpinners(){
 
@@ -89,39 +99,64 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             fromSpinner.adapter = aa
 
+
+
             toSpinner = findViewById(R.id.toSpinner)
             this.also { it.also { toSpinner.onItemSelectedListener = it } }
 
             val bb = ArrayAdapter(this, android.R.layout.simple_spinner_item, locationsName)
             bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             toSpinner.adapter = bb
+
+
+            flightFromSpinner = findViewById(R.id.flightFromSpinner)
+            this.also { it.also { flightFromSpinner.onItemSelectedListener = it } }
+
+            val aaf = ArrayAdapter(this, android.R.layout.simple_spinner_item, locationsName)
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            flightFromSpinner.adapter = aaf
+
+
+
+            flightToSpinner = findViewById(R.id.flightToSpinner)
+            this.also { it.also { flightToSpinner.onItemSelectedListener = it } }
+
+            val bbf = ArrayAdapter(this, android.R.layout.simple_spinner_item, locationsName)
+            bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            flightToSpinner.adapter = bbf
+
         }
         else{
             Toast.makeText(this,"Failed session",Toast.LENGTH_LONG).show()
         }
 
 
-
     }
 
     fun findTicket(view: View) {
-
-
         val originId =  viewModel.idleArray[fromSpinner.selectedItemPosition]
         val destinationId =  viewModel.idleArray[toSpinner.selectedItemPosition]
-        originId?.let {
-            destinationId?.let { it1 ->
 
-                val intent = Intent(this, JourneyActivity::class.java)
-                intent.putExtra("sessionId", viewModel.idleSessionId)
-                intent.putExtra("deviceId", viewModel.idleDeviceId)
-                intent.putExtra("originId", it)
-                intent.putExtra("destinationId", it1)
-                intent.putExtra("date", dateFor)
-                startActivity(intent)
+        if(originId?.equals(destinationId) == false)
+        {
+            originId?.let {
+                destinationId?.let { it1 ->
 
+                    val intent = Intent(this, JourneyActivity::class.java)
+                    intent.putExtra("sessionId", viewModel.idleSessionId)
+                    intent.putExtra("deviceId", viewModel.idleDeviceId)
+                    intent.putExtra("originId", it)
+                    intent.putExtra("destinationId", it1)
+                    intent.putExtra("date", dateFor)
+                    startActivity(intent)
+
+                }
             }
         }
+        else{
+            Toast.makeText(this,"Aynı yerden aynı yere gidemezsiniz!", Toast.LENGTH_LONG).show()
+        }
+
     }
 
     fun setToday(view: View){
@@ -139,8 +174,26 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         val idle2 :Int = toSpinner.selectedItemPosition
         fromSpinner.setSelection(idle2)
         toSpinner.setSelection(idle)
+
+        flightFromSpinner.setSelection(idle2)
+        flightToSpinner.setSelection(idle)
     }
 
+    fun removeReturn(view: View)
+    {
+        returnSection.visibility =View.GONE
+    }
+
+    fun busSection(view: View) {
+        swipeRefresh.visibility = View.VISIBLE
+        swipeRefreshFlight.visibility = View.GONE
+    }
+
+    fun flightSection(view: View) {
+        println("plane kanka")
+        swipeRefresh.visibility = View.GONE
+        swipeRefreshFlight.visibility = View.VISIBLE
+    }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
